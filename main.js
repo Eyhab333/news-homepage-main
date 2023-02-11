fa2e5267ba8712edd5372d0be0f600bc3b06ac07
Eyhab333 <107456407+Eyhab333@users.noreply.github.com>
2023-02-11 19:32:06 +0300

    phone

--- main.js
+++ main.js
@@ -2,12 +2,10 @@ const hamburger = document.querySelector(".hamburger");
 const close = document.querySelector(".close");
 const topnav = document.querySelector(".topnav");
 
-
 hamburger.addEventListener("click", () => {
   hamburger.style.display = "none"
   close.style.display = "block"
   topnav.style.display = "block"
-  document.body.style.backgroundColor = "hsla(240, 100%, 5%, 0.5)";
 })
 
 close.addEventListener("click", () => {
